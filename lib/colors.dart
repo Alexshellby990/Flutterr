a82f3a94038e7dd6879aa60a5f450908ee3c3c3c
Mubashar Mumtaz <101728912+Alexshellby990@users.noreply.github.com>
2023-01-10 23:03:29 +0500

    Add files via upload

---
+++ lib/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class MyColors {
+  static const primaryColor = Color(0xffb86420);
+  static const orangeTile = Color(0xfff00505);
+  static const orangeDivider = Color(0xffffc77e);
+  static const contactDivider = Color(0xffb24680);
+  static const drawalDivider = Color(0xffb02468);
+  static const drawalBackground = Color(0xff141414);
+}
